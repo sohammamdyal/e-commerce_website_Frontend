906462d683003912f2e42c788a9ac5cd56b36709
soham pankaj mamdyal <75490885+sohammamdyal@users.noreply.github.com>
2025-11-21 17:59:22 +0530

    updated apijs file

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const BASE = "http://localhost:4000";
 // ✅ Fetch all products (with filters)
 export async function fetchProducts(params = {}) {
     const query = new URLSearchParams(params).toString();
-    const res = await fetch(`http://localhost:4000/api/products?${query}`);
+    const res = await fetch(`https://e-commerce-website-backend-2-mxpy.onrender.com/api/products?${query}`);
     const data = await res.json();
   
     return Array.isArray(data) ? data : [];
@@ -12,13 +12,13 @@ export async function fetchProducts(params = {}) {
 
 // ✅ Fetch single product
 export async function fetchProductById(id) {
-  const res = await fetch(`${BASE}/api/products/${id}`);
+  const res = await fetch(`https://e-commerce-website-backend-2-mxpy.onrender.com/api/products/${id}`);
   return await res.json();
 }
 
 // ✅ Fetch categories
 export async function fetchCategories() {
-    const res = await fetch("http://localhost:4000/api/categories");
+    const res = await fetch("https://e-commerce-website-backend-2-mxpy.onrender.com/api/categories");
     const data = await res.json();
     return Array.isArray(data) ? data : [];
   }
